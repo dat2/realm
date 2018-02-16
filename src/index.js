3f84056d7bd6012aead8065fd4c8670a3cd24c3e
Nicholas Dujay <nickdujay@gmail.com>
2018-02-15 21:54:43 -0500

    split react component to its own thing

--- src/index.js
+++ src/index.js
@@ -2,39 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-import Relm from './relm';
+import Relm, { Cmd, Pair, Random } from './relm';
 
 const model = {
   num: 0,
-  text: ''
+  text: '',
+  face: 1
 };
 
 const update = msg => model => {
   switch (msg.type) {
     case 'Increment':
-      return { ...model, num: model.num + 1 };
+      return Pair({ ...model, num: model.num + 1 }, Cmd.none);
     case 'Decrement':
-      return { ...model, num: model.num - 1 };
+      return Pair({ ...model, num: model.num - 1 }, Cmd.none);
     case 'Change':
-      return { ...model, text: msg.value };
+      return Pair({ ...model, text: msg.value }, Cmd.none);
+    case 'Roll':
+      return Pair(model, Random.generate('NewFace')(Random.int(1)(6)));
+    case 'NewFace':
+      return Pair({ ...model, face: msg.value }, Cmd.none);
     default:
-      return model;
+      return Pair(model, Cmd.none);
   }
-}
+};
 
 ReactDOM.render(
   <Relm model={model} update={update}>
-    {
-      ({ model, onClick, onChange }) => (
-        <div>
-          <button onClick={onClick('Decrement')}>-</button>
-          <div>{ model.num }</div>
-          <button onClick={onClick('Increment')}>+</button>
-          <input value={model.text} onChange={onChange('Change')} />
-          <p>{ model.text.split('').reverse().join('') }</p>
-        </div>
-      )
-    }
-  </Relm>
-, document.getElementById('root'));
+    {({ model, onClick, onChange }) => (
+      <div>
+        <button onClick={onClick('Decrement')}>-</button>
+        <div>{model.num}</div>
+        <button onClick={onClick('Increment')}>+</button>
+        <input value={model.text} onChange={onChange('Change')} />
+        <p>
+          {model.text
+            .split('')
+            .reverse()
+            .join('')}
+        </p>
+        <button onClick={onClick('Roll')}>Roll</button>
+        <p>{model.face}</p>
+      </div>
+    )}
+  </Relm>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
--- src/relm.js
+++ src/relm.js
@@ -1,47 +1,101 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+export const Cmd = {
+  none: { type: 'none' }
+};
+
+export const Random = {
+  generate: msg => generator => ({
+    type: 'Random.generate',
+    msg,
+    generator
+  }),
+  int: min => max => ({
+    type: 'int',
+    min,
+    max
+  })
+};
+
+export const Pair = (left, right) => ({
+  type: 'Pair',
+  left,
+  right
+});
+
+class RelmStore {
+  constructor({ model, update }) {
+    this.model = model;
+    this.update = update;
+    this.subscribers = [];
+  }
+
+  dispatch(msg) {
+    const result = this.update(msg)(this.model);
+    if (result.type === 'Pair') {
+      this.model = result.left;
+      this.handleCmd(result.right);
+    } else {
+      this.model = result;
+    }
+    this.subscribers.forEach(subscriber => {
+      subscriber();
+    });
+  }
+
+  handleCmd(cmd) {
+    if (cmd.type === 'Random.generate') {
+      if (cmd.generator.type === 'int') {
+        const randomNumber =
+          Math.floor(
+            Math.random() *
+              Math.floor(cmd.generator.max + 1 - cmd.generator.min)
+          ) + cmd.generator.min;
+        this.dispatch({ type: cmd.msg, value: randomNumber });
+      }
+    }
+  }
+
+  subscribe(subscriber) {
+    this.subscribers.push(subscriber);
+    return () => {
+      const index = this.subscribers.indexOf(subscriber);
+      this.subscribers.splice(index, 1);
+    };
+  }
+}
+
 export default class Relm extends React.Component {
   static propTypes = {
-    children: PropTypes.func.isRequired,
-  }
+    children: PropTypes.func.isRequired
+  };
 
   constructor(props) {
     super(props);
-    this.state = {
-      model: props.model
-    };
+    this._relm = new RelmStore(props);
+    this._unsubscribe = this._relm.subscribe(() => {
+      this.setState({});
+    });
   }
 
-  dispatch = msg => {
-    this.setState({
-      model: this.props.update(msg)(this.state.model)
-    })
+  componentWillUnmount() {
+    this._unsubscribe();
   }
 
   onClick = msg => e => {
-    if (typeof msg === typeof '') {
-      this.dispatch({ type: msg, e });
-    } else {
-      this.dispatch(msg(e));
-    }
-  }
+    this._relm.dispatch({ type: msg, e });
+  };
 
   onChange = msg => e => {
-    if (typeof msg === typeof '') {
-      this.dispatch({ type: msg, value: e.target.value });
-    } else {
-      this.dispatch(msg(e.target.value));
-    }
-  }
+    this._relm.dispatch({ type: msg, value: e.target.value });
+  };
 
   render() {
-    return (
-      this.props.children({
-        model: this.state.model,
-        onClick: this.onClick,
-        onChange: this.onChange
-      })
-    )
+    return this.props.children({
+      model: this._relm.model,
+      onClick: this.onClick,
+      onChange: this.onChange
+    });
   }
 }
